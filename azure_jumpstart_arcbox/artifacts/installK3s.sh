836632569c2730bb961cc7284a671a8f0d324080
Zaid Mohammad <mozaid@microsoft.com>
2024-02-23 08:31:29 -0500

    remove error handling

--- azure_jumpstart_arcbox/artifacts/installK3s.sh
+++ azure_jumpstart_arcbox/artifacts/installK3s.sh
@@ -141,10 +141,6 @@ if [[ "$k3sControlPlane" == "true" ]]; then
     resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$stagingStorageAccountName']".[resourceGroup] --resource-type "Microsoft.Storage/storageAccounts" -o tsv)
     workspaceResourceId=$(sudo -u $adminUsername az resource show --resource-group $resourceGroup --name $logAnalyticsWorkspace --resource-type "Microsoft.OperationalInsights/workspaces" --query id -o tsv)
     sudo -u $adminUsername az connectedk8s connect --name $vmName --resource-group $resourceGroup --location $location --tags 'Project=jumpstart_arcbox' --only-show-errors
-    if [[ $? -ne 0 ]]; then
-        echo "ERROR: Failed to onboard the cluster to Azure Arc"
-        exit 1
-    fi
 
     # Enabling Container Insights and Microsoft Defender for Containers cluster extensions
     echo ""
