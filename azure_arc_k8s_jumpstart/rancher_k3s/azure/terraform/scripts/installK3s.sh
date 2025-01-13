b3c6c995f90994915a3b87278952ee34ddf25d87
Zaid Mohammad <mozaid@microsoft.com>
2025-01-12 19:27:29 -0500

    disable container insights

--- azure_arc_k8s_jumpstart/rancher_k3s/azure/terraform/scripts/installK3s.sh
+++ azure_arc_k8s_jumpstart/rancher_k3s/azure/terraform/scripts/installK3s.sh
@@ -100,4 +100,4 @@ sudo -u $adminUsername az login --service-principal --username $appId --password
 echo ""
 resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$vmName']".[resourceGroup] --resource-type "Microsoft.Compute/virtualMachines" -o tsv)
 sudo -u $adminUsername az connectedk8s connect --name $vmName --resource-group $resourceGroup --location $azureLocation --kube-config /home/${adminUsername}/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --enable-oidc-issuer --enable-workload-identity --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
-sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name $vmName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
+# sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name $vmName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
